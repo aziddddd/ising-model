ffc5bf0fd7692865b9ec654ec86f943129cd745a
azidharun-msi <azidharun@gmail.com>
2019-01-21 02:13:05 +0800

    update visualisation process

--- notebooks/im_utils.py
+++ notebooks/im_utils.py
@@ -3,43 +3,23 @@ from numpy.random import rand
 
 import matplotlib.pyplot as plt
 from matplotlib import animation
+from IPython.display import clear_output
 
 from numba import jit
-
+from tqdm import tqdm
 
 # Class to sample an equilibrium state of the Ising model. ( Glauber / Kawasaki )
-
 class Ising():
+
+############################################### INITIALISER #####################################################
+
     def __init__(self, N, temp, method):
         self.N          = N
         self.beta       = 1.0/temp
         self.method     = method
         self.config     = 2*np.random.randint(2, size=(N,N))-1  
 
-    @jit
-    def Glauber(self):
-        for i in range(self.N):
-            for j in range(self.N):
-                # Choose a site i at random
-                row, col    = np.random.randint(0, self.N), np.random.randint(0, self.N)
-                i           = self.config[row, col]
-
-                # Calculate ∆E
-                neighbours  = self.config[(row+1)%self.N,col] + self.config[row,(col+1)%self.N] + self.config[(row-1)%self.N,col] + self.config[row,(col-1)%self.N]                     # BOTTOM + RIGHT + LEFT + TOP
-                delta_e     = 2*i*neighbours                                                                                                                                            # ∆E = E_nu - E_mu = 2E_nu
-
-                # If ∆E ≤ 0, spin flip always flipped
-                if delta_e < 0:
-                    i *= -1
-
-                # Else, flipped with P = exp(-∆E/kT)
-                elif rand() < np.exp(-delta_e*self.beta):
-                    i *= -1
-                self.config[row, col] = i
-        return self.config
-            # Consider the effect of flipping the spin
-
-            # Write down expression of ∆E for the choice site i ( How many spin variables enter this expression)
+############################################## CHOOSE METHOD #####################################################
 
     def montecarlo(self):
         while True:
@@ -52,20 +32,7 @@ class Ising():
                 self.method = (input('Which method? (glauber/kawasaki)'))
         return self.config
 
-    def simulate(self, i):
-        print('Frame : {}'.format(i))
-        self.montecarlo()
-        X, Y = np.meshgrid(range(self.N), range(self.N))
-        quad = plt.pcolormesh(X, Y, self.config, cmap=plt.cm.YlGn)
-        plt.title('Time=%d'%i); plt.axis('tight')
-        quad.set_array(self.config.ravel())
-        return quad
-        # plt.close()
-
-    # def animate(self):
-    #     plotFigure = plt.figure(figsize=(5, 5), dpi=80)
-    #     anim = animation.FuncAnimation(plotFigure, self.simulate, frames=10, interval=50)
-    #     return anim
+################################################## METHOD ########################################################
 
     # def Kawasaki():
         # Choose randomly 2 dinstinct sites i & j
@@ -79,11 +46,75 @@ class Ising():
 
         # Consider the exchange as two consecutive sigle spin flips, so ∆E sum of E changes for 2 moves separately.
 
-# N           = 50
-# temp        = .4
-# method      = 'glauber'
+    @jit
+    def Glauber(self):
+        for i in range(self.N):
+            for j in range(self.N):
+                # Choose a site i at random
+                row, col    = np.random.randint(0, self.N), np.random.randint(0, self.N)
+                i           = self.config[row, col]
 
-# ising_model = Ising(N, temp, method)
-# plotFigure = plt.figure(figsize=(10, 10), dpi=80)
-# ani = animation.FuncAnimation(plotFigure, ising_model.simulate, interval=0)
-# plt.show()
+                # Calculate ∆E
+                neighbours  = self.config[(row+1)%self.N,col] + self.config[row,(col+1)%self.N] + self.config[(row-1)%self.N,col] + self.config[row,(col-1)%self.N]         # BOTTOM + RIGHT + LEFT + TOP
+                delta_e     = 2*i*neighbours                                                                                                                                # ∆E = E_nu - E_mu = 2E_nu
+
+                # If ∆E ≤ 0, spin flip always flipped
+                if delta_e < 0:
+                    i *= -1
+
+                # Else, flipped with P = exp(-∆E/kT)
+                elif rand() < np.exp(-delta_e*self.beta):
+                    i *= -1
+                self.config[row, col] = i
+        return self.config
+
+################################################### ANIMATION #######################################################
+
+    def simulate(self, i):
+        print('Frame : {}'.format(i))
+        self.montecarlo()
+        X, Y = np.meshgrid(range(self.N), range(self.N))
+        quad = plt.pcolormesh(X, Y, self.config, cmap=plt.cm.YlGn)
+        plt.title('Time=%d'%i); plt.axis('tight')
+        quad.set_array(self.config.ravel())
+        return quad
+
+
+################################################### SNAPSHOTS #######################################################
+
+    @jit
+    def snapshots(self, numstep):
+        for i in tqdm(range(numstep)):
+            self.montecarlo()
+            if i == numstep/numstep :          self.plotStep( i, 1);
+            if i == numstep/20      :          self.plotStep( i, 2);
+            if i == numstep/10      :          self.plotStep( i, 3);
+            if i == numstep/5       :          self.plotStep( i, 4);
+            if i == numstep/2       :          self.plotStep( i, 5);
+            if i == numstep-1       :          self.plotStep( i, 6);
+                 
+    @jit                    
+    def plotStep(self, i, n_):
+        X, Y = np.meshgrid(range(self.N), range(self.N))
+        plt.subplot(3, 3, n_, xticklabels=[], yticklabels=[])
+        plt.pcolormesh(X, Y, self.config, cmap=plt.cm.YlGn);
+        plt.title('Time=%d'%i); plt.axis('tight')
+        plt.tight_layout()
+    plt.show()
+
+################################################### DYNAMIC PLOT #####################################################
+
+    @jit
+    def DynamicPlot(self, numstep):
+        for i in range(numstep):
+            self.montecarlo()
+            self.dynamicplotStep(i)
+
+    @jit                                     
+    def dynamicplotStep(self, i):
+        X, Y = np.meshgrid(range(self.N), range(self.N))
+        clear_output(wait=True)
+        plt.figure(figsize=(5, 5), dpi=80)   
+        plt.pcolormesh(X, Y, self.config, cmap=plt.cm.YlGn);
+        plt.title('Time=%d'%i); plt.axis('tight')
+        plt.show()
